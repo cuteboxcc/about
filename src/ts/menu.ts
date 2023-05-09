24bd8ad26332a036e58ab87e5abc3f8a46bb1878
koyu <me@koyu.space>
2023-05-09 22:20:32 +0200

    Add active menu links

---
+++ src/ts/menu.ts
@@ -0,0 +1,12 @@
+import $ from "jquery";
+
+const domain = location.href.split("/")[2];
+let protocol = "https://";
+
+if (domain.startsWith("localhost")) {
+    protocol = "http://";
+}
+
+let page = location.href.replace(/\/+$/, '').replace(protocol+domain+"/", "").replaceAll("/", "--").split("?")[0];
+
+$(`#nav-${page.replaceAll("#", "")}`).attr("class", "nav-link active");
